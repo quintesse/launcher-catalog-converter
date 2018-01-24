418b8552e85dc8ac725c0e6abc4c2564e5a79102
Tako Schotanus <tako@ceylon-lang.org>
2018-01-24 04:22:03 +0100

    Removed obsolete code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,8 +29,6 @@ public class Main {
     private static final String ENV_PRODUCTION = "production";
     private static final String ENV_STAGING = "staging";
     private static final String ENV_DEVELOPMENT = "development";
-    public static final File CACHE_DIR = new File("target/booster-catalog-cache");
-    public static final File DEST_DIR = new File("target/converted-booster-catalog");
     
     public static void main(String[] args) throws Exception {
         if (args.length == 0 || (args.length == 1 && args[0].equals("--help"))) {
